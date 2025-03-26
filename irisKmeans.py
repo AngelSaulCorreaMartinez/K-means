7a455ee1ff33402370545007070c37dce8ee0604
ASCM5 <utm22030590@utma.edu.mx>
2025-03-26 13:31:37 -0600

    Codigo completo

--- irisKmeans.py
+++ irisKmeans.py
@@ -35,13 +35,13 @@ iris_df = iris_df.dropna()
 #Reestablecemos el indice y eliminamos el indice anterior (drop true)
 iris_df = iris_df.reset_index(drop=True)
 
-########################## FIN LIMPIEZA DE DATA FRAME ###############################
-
 # Guardo y elimino datos por aparte
 names = iris_df['name']
 target = iris_df['class']
 
 iris_df = iris_df.drop(['name', 'class'], axis=1) #1==columnas, 0==filas
+
+########################## FIN LIMPIEZA DE DATA FRAME ###############################
 
 
 ########################## NORMALIZACION DE DATOS ###############################
@@ -68,11 +68,11 @@ wcss = [] # Es una lista vacía que almacenará los valores de Within-Cluster-Su
 #For para probar diferentes cantidades de clusters
 for i in range(2,11):
     # kmeans = KMeans(n_clusters=i, init='random', max_iter=300, n_init=10, random_state=0) 
-    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0) 
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=1, random_state=0) 
     #init selecciona el metodo a usar para seleccionar los centroides iniciales, max_iter limita las iteraciones por cada inicializacion
     # n_init nos indica el total de inicializaciones que tendremos, random_state fija la semilla de aleatoriedad
     # al usar k-means++ no se recomeinda usar random_state con un n_init > 1
-    # en caso contrario cada iteracion dara el mismo resultado causando redundancia 
+    # en caso contrario probablemente cada iteracion dara el mismo resultado causando redundancia 
 
     #Ajustamos el modelo a los datos (entrenar el modelo)
     kmeans.fit(df_escalado)
@@ -84,7 +84,55 @@ for i in range(2,11):
     print(f'Inercia calculada: {kmeans.inertia_}')
 
 plt.plot(range(2,11), wcss, marker='o', color='red')
+plt.title('Metodo de Elbow')
+plt.xlabel('Numero de Clusters')
+plt.ylabel('WCSS')
 plt.show()
+
+########################## FIN METODO DEL CODO ###############################
+
+
+########################## ENTRENAR MODELO CON NUMERO DE CLUSTERS SELECCIONADO ###############################
+
+#Aqui inicializo mas veces sin importar la redundancia ya que busco que se ajuste lo mas posible
+kmeans = KMeans(n_clusters=int(input('Ingresa el numero de Clusters: ')), init='k-means++', n_init=10, max_iter=300, random_state=0 )
+kmeans.fit(df_escalado)
+#print(f'--------------------------\nInercia calculada: {kmeans.inertia_}')
+
+########################## FIN ENTRENAR MODELO CON NUMERO DE CLUSTERS SELECCIONADO ###############################
+
+
+
+########################## GRAFICAS ###############################
+
+#Obtenemos los label de cada cluster
+cluster_labels = kmeans.labels_
+#print(f'Cluster labels: {cluster_labels}')
+
+#Definimos los colores para cada cluster
+diccionario_colores = {0:'black', 1:'purple', 2:'blue', 3:'orange', 4:'pink', 5:'yellow'}
+colores_clusters = [diccionario_colores[i] for i in cluster_labels]
+#print(colores_clusters)
+
+#Visualizacion de colores y especies
+indice = 0
+for i in iris.target_names:
+    print(f'{i} : {diccionario_colores.get(indice)}')
+    indice += 1
+
+#Graficamos los clusters
+plt.scatter(df_escalado.iloc[:, 0], df_escalado.iloc[:, 1], c=colores_clusters, s=50, alpha=0.5)
+#Graficamos los centroides
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x', s=200, label='Centroides')
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
+plt.title(f'K-means Clustering')
+plt.show()
+
+plt.scatter(iris_df.iloc[:, 0], iris_df.iloc[:, 1], c=colores_clusters, s=50, alpha=0.5)
+plt.show()
+########################## FIN GRAFICAS ###############################
+
 
 
 # print(names, target)
